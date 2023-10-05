4059e59cf4fd29b72324021cf6c6c97929d4649e
essaket <hichamessaket17gmail.com>
2023-10-05 13:30:59 -0700

    Done

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: My first source code in C program
+ *
+ * Return:  Always 0 for Success
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
