a6e1b942338d6be17e8e06676d04cecf670fbb6f
essaket <hichamessaket17gmail.com>
2023-10-06 13:42:54 -0700

    Done

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,27 @@
+
+#include <stdio.h>
+
+/**
+ *  main - Entry point
+ *
+ *  Description: prints all the numbers of base 16 in lowercase
+ *
+ *  Return: Always 0 for Success
+ */
+
+int main(void)
+{
+	int i = 48; /*48 is decimal rep of 0*/
+
+	while (i <= 120) /*120 is decimal rep of f*/
+	{
+		putchar(i);
+
+		if (i == 57)
+			i += 39;
+		i++;
+	}
+	putchar('\n');
+
+	return (0);
+}
