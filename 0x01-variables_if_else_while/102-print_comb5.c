42467589707a3115820e6f63ae72eea41cb3a3aa
essaket <hichamessaket17gmail.com>
2023-10-06 15:42:51 -0700

    Done

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -23,7 +23,7 @@ int main(void)
 				putchar(' ');
 				putchar((y / 10) + 48);
 				putchar((y % 10) + 48);
-				if (x != 98 || y != 99)
+				if (x != 98 && y != 99)
 				{
 					putchar(',');
 					putchar(' ');
