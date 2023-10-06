077267a4906d7ddd030af7625d83e1aa99a5fba3
essaket <hichamessaket17gmail.com>
2023-10-06 15:07:51 -0700

    Done

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -12,14 +12,15 @@ int main(void)
 {
 	int x, y;
 
-	for (x = 48 ; x <= 56 ; x++ )
+	for (x = 48; x <= 56; x++)
 	{
-		for (y = 49 ; y <= 57 ; y++)
-			if ( x != y && x < y)
+		for (y = 49; y <= 57; y++)
+		{
+			if (y > x)
 			{
 				putchar(x);
 				putchar(y);
-				if (x != 56 && y != 57)
+				if (x != 56 || y != 57)
 				{
 					putchar(',');
 					putchar(' ');
@@ -28,6 +29,5 @@ int main(void)
 		}
 	}
 	putchar('\n');
-
 	return (0);
 }
