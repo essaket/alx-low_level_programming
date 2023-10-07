9ba9cdccfe5566112febf5785df7fb8e975fbba8
essaket <hichamessaket17gmail.com>
2023-10-06 17:37:18 -0700

    Done

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -10,28 +10,30 @@
 
 int main(void)
 {
-	int x, y;
+        int x = 0, y;
 
-	for (x = 0; x <= 99; x++)
-	{
-		for (y = 0; y < 100; y++)
-		{
-			if (x < y)
-			{
-				putchar((x / 10) + 48);
-				putchar((x % 10) + 48);
-				putchar(' ');
-				putchar((y / 10) + 48);
-				putchar((y % 10) + 48);
-				if (x != 98 || y != 99)
-				{
-					putchar(',');
-					putchar(' ');
-				}
-			}
-
-		}
-	}
-	putchar('\n');
-	return (0);
+        while (x <= 99)
+        {
+        	y = x;
+                while (y < 100)
+                {
+                        if (x < y)
+                        {
+                                putchar((x / 10) + 48);
+                                putchar((x % 10) + 48);
+                                putchar(' ');
+                                putchar((y / 10) + 48);
+                                putchar((y % 10) + 48);
+                                if (x != 98 || y != 99)
+                                {
+                                        putchar(',');
+                                        putchar(' ');
+                                }
+                        }
+                        y++;
+                }
+                x++;
+        }
+        putchar('\n');
+        return (0);
 }
