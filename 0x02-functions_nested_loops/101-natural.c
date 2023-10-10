1907627f7884ec5009aff9f50b2291e4232d9750
essaket <hichamessaket17gmail.com>
2023-10-10 13:34:20 -0700

    Done

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -10,15 +10,14 @@
 
 int main(void)
 {
-	int x;
-	short sum;
+	int x, sum;
 
 	for (x = 0; x < 1024; x++)
 	{
 		if ((x % 3) == 0 || (x % 5) == 0)
 			sum += x;
 	}
-	printf("%hd\n", sum);
+	printf("%d\n", sum);
 
 	return (0);
 }
