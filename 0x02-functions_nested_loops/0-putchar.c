b33d05ba07ca1f806fb779400f2612676394b670
essaket <hichamessaket17gmail.com>
2023-10-10 05:35:39 -0700

    Done

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ *  main - Entry point
+ *
+ *  Description: program that print _putchar.
+ *
+ *  Return: Always 0 for Success
+ */
+
+int main(void)
+{
+	putchar('_');
+	putchar('p');
+	putchar('u');
+	putchar('t');
+	putchar('c');
+	putchar('h');
+	putchar('r');
+	putchar('\n');
+	return (0);
+}
