b51381a419b7bc39de8241472bdbdef3db0c8288
essaket <hichamessaket17gmail.com>
2023-10-12 06:28:40 -0700

    Done

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -11,13 +11,13 @@ void more_numbers(void)
 {
 	int x, y;
 
-	for (x = 1; x <= 10; x++)
+	for (x = 0; x <= 9; x++)
 	{
 		for (y = 0; y <= 14; y++)
 		{
-			if (i > 9)
+			if (y > 9)
 				_putchar(1);
-			_putchar(y % 10 + '0');
+			_putchar((y % 10) + '0');
 		}
 		_putchar('\n');
 	}
