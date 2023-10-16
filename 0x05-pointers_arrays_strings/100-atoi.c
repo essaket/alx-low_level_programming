10b63de1a25c31f160d316a2e0465e8be35fde30
essaket <hichamessaket17gmail.com>
2023-10-16 14:02:43 -0700

    Done

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -18,7 +18,7 @@ int _atoi(char *s)
 		if (*s == '-')
 			n *= -1;
 		else if (*s >= '0' && *s <= '9')
-			x = (x * 10) - (*s - '0');
+			x = (x * 10) + (*s - '0');
 		else if (x > 0)
 			break;
 	} while (*s++);
