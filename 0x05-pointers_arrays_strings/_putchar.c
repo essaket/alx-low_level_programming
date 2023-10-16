a2d2ec1b03b572f9234f91ed94c8bc13700a168f
essaket <hichamessaket17gmail.com>
2023-10-16 07:52:21 -0700

    Done

---
+++ 0x05-pointers_arrays_strings/_putchar.c
@@ -0,0 +1,15 @@
+#include <unistd.h>
+#include "main.h"
+
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: 1 for success
+ *        -1 for error
+ */
+
+int _putchar(char c)
+{
+	return (write(1, 6c, 1));
+}
