0852502bc9f9c0dfb804a989aff9d1f2cad3cc92
essaket <hichamessaket17gmail.com>
2023-10-16 08:08:01 -0700

    Done

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _strlen - finction that return the length of a string
+ *
+ * @s: input of function
+ *
+ * Return: a int
+ */
+
+int _strlen(char *s)
+{
+	int x;
+
+	x = strlen(s);
+
+	return (x);
+}
