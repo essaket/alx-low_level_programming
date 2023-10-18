2bdea06b8af922c9cd0ec5b47404e8daad32ce02
essaket <hichamessaket17gmail.com>
2023-10-18 10:25:42 -0700

    Done

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -12,16 +12,12 @@
 void reverse_array(int *a, int n)
 {
 	int x, c;
-	int y = 0;
-
-	while (a[y] != '\0')
-		y++;
 
 	for (x = 0; x < n - 1; x++)
 	{
 		c = a[x];
-		a[x] = a[y - x];
-		a[y - x] = c;
+		a[x] = a[n];
+		a[n] = c;
 		n--;
 	}
 }
