da39bed1aa9fbaa6196d66b74e3e3d66d53dbd55
essaket <hichamessaket17gmail.com>
2023-10-18 12:20:22 -0700

    Done

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * cap_string - function that capitalizes all words of a string
+ *
+ * @x: input pointer of function
+ *
+ * Return: a string
+ */
+
+char *cap_string(char *x)
+{
+	int i = 0;
+
+	while (x[i])
+	{
+		while(!(x[i] > 96 && x[i] < 123))
+			i++;
+	
+		if (x[i - 1] == ',' || x[i - 1] == ' ' || x[i - 1] == '\t' || x[i - 1] == '\n' || x[i - 1] == ';' || x[i - 1] == '.' ||
+				x[i - 1] == '!' || x[i - 1] == '?' || x[i - 1] == '"' || x[i - 1] == '(' || x[i - 1] == ')' || x[i - 1] == '{' ||
+				x[i - 1] == '}')
+				x[i] -= 32;
+		i++;
+	}
+
+	return (x);
+}
