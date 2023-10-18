139bbb682bb30321030ab725a2b0babb6a922d26
essaket <hichamessaket17gmail.com>
2023-10-18 12:48:45 -0700

    Done

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -3,25 +3,35 @@
 /**
  * cap_string - function that capitalizes all words of a string
  *
- * @x: input pointer of function
+ * @str: input pointer of function
  *
  * Return: a string
  */
 
-char *cap_string(char *x)
+char *cap_string(char *str)
 {
-	int i = 0;
+	int x = 0;
 
-	while (x[i])
+	while (str[x])
 	{
-		while (!(x[i] > 96 && x[i] < 123))
-			i++;
-		if (x[i - 1] == ',' || x[i - 1] == ' ' || x[i - 1] == '\t' ||
-			x[i - 1] == '\n' || x[i - 1] == ';' || x[i - 1] == '.' ||
-			x[i - 1] == '!' || x[i - 1] == '?' || x[i - 1] == '"' ||
-			x[i - 1] == '(' || x[i - 1] == ')' || x[i - 1] == '{' ||
-			x[i - 1] == '}' || x == 0)
-			x[i] -= 32;
+		while (!(str[x] >= 'a' && str[x] <= 'z'))
+			x++;
+
+		if (str[x - 1] == ' ' ||
+			str[x - 1] == '\t' ||
+			str[x - 1] == '\n' ||
+			str[x - 1] == ',' ||
+			str[x - 1] == ';' ||
+			str[x - 1] == '.' ||
+			str[x - 1] == '!' ||
+			str[x - 1] == '?' ||
+			str[x - 1] == '"' ||
+			str[x - 1] == '(' ||
+			str[x - 1] == ')' ||
+			str[x - 1] == '{' ||
+			str[x - 1] == '}' ||
+			x == 0)
+			str[x] -= 32;
 		i++;
 	}
 
