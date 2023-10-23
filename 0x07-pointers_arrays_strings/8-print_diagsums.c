73ca4f61e79ad03620ea5de9dbc0a56bad275149
essaket <hichamessaket17gmail.com>
2023-10-23 11:28:54 -0700

    Done 8-print_diagsums.c

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -7,14 +7,13 @@
  * @a: an array
  * @size: input of function
  *
- * Return: 0 for success
  */
 
 void print_diagsums(int *a, int size)
 {
-	int i = 0, sum1 = 0, sum2 = 0;
+	int i, sum1 = 0, sum2 = 0;
 
-	for (; i < size; i++)
+	for (i = 0; i < size; i++)
 	{
 		sum1 += a[i];
 		sum2 += a[size - i - 1];
