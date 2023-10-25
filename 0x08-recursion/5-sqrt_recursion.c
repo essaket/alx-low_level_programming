909fe180a8a8089b1a6f0760a5e270c38bd09a34
essaket <hichamessaket17gmail.com>
2023-10-25 09:26:21 -0700

    Done 5-sqrt_recursion.c

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -4,22 +4,36 @@
  * _sqrt_recursion - function that returns the natural square root of a number
  *
  * @n: input of function
+ * @x: input of function
  *
  * Return: a square root
  */
 
+int calcul_sqrt_recursion(int n, int x);
+
 int _sqrt_recursion(int n)
 {
-	int i = 0;
+	return (calcul_sqrt_recursion(n, 1));
+}
 
-	if (i * i == n)
-		return (i);
+/**
+ * calcul_sqrt_recursion - function that calcul the natural square root of n
+ *
+ * @n: input of function
+ * @x: input of function
+ *
+ * Return: calcul of natural square root of n
+ */
 
-	else if (i * i < n)
-	{
-		i++;
-		return (_sqrt_recursion(n));
-	}
+int calcul_sqrt_recursion(int n, int x)
+{
+	if (x * x == n)
+		return (x);
 
-	return (i);
+	else if (x * x < n)
+		return (calcuö_sqr_recirsion(n, x + 1));
+
+	else
+		return (-1);
+
 }
