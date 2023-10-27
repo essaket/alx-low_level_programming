802acfeb73b040de736e1dbbe4617e030cb5b6aa
essaket <hichamessaket17gmail.com>
2023-10-27 04:57:57 -0700

    Done 1-args.c

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * main - program that prints the number of arguments passed into it
+ *
+ * @argc: input size of arguments
+ * @argv: input array of arguments
+ *
+ * Return: 0 for success
+ */
+
+int main(int argc, char *argv)
+{
+	printf("%d\n", argc);
+	(void)argv;
+
+	return (0);
+}
