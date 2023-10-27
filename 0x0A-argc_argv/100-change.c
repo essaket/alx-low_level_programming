c5ab258712ed9d9754a3a3d876b6001b26cae1d7
essaket <hichamessaket17gmail.com>
2023-10-27 09:01:53 -0700

    Done 100-change.c

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -11,27 +11,32 @@
  *
  * Return: 0 for success
  */
+
 int main(int argc, char *argv[])
 {
-        int i, c = 0, m = atoi(argv[1]);
-        int cent[] = {25, 10, 5, 2, 1};
-
-        if (argc == 2)
-        {
-                for (i = 0; i < 5; i++)
-                {
-                        if (m >= cent[i])
-                        {
-                                c += m / cent[i];
-                                m = m % cent[i];
-                                if (m % cent[i] == 0)
-                                        break;
-                        }
-                }
-                printf("%d\n", c);
-        }
-        else
-                return (printf("Error\n"), 1);
-
-        return (0);
+	if (argc == 2)
+	{
+		int i, c = 0, m = atoi(argv[1]);
+		int cent[] = {25, 10, 5, 2, 1};
+		
+		for (i = 0; i < 5; i++)
+		{
+			if (m >= cent[i])
+			{
+				c += m / cent[i];
+				m = m % cent[i];
+				if (m % cent[i] == 0)
+				{
+					break;
+				}
+			}
+		}
+		printf("%d\n", c);
+	}
+	else
+	{
+		printf("Error\n");
+		return (1);
+	}
+	return (0);
 }
