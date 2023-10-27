289cea0d6ad0945a3155c597791af975a0ebbdde
essaket <hichamessaket17gmail.com>
2023-10-27 04:48:29 -0700

    Done 0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * main - program that prints it's name
+ *
+ * @argc: input size of argv
+ * @argv: input pointer of function
+ *
+ * Return: 0 for success
+ */
+
+int main(int argc __attribute__((unused)), char *argv[])
+{
+	printf("%S\n", *argv[]);
+
+	return (0);
+}
