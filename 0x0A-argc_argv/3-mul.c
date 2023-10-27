339194b90e9a71bd3094bba5500a3ead70e3edcf
essaket <hichamessaket17gmail.com>
2023-10-27 06:52:06 -0700

    Done 3-mul.c

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * main - program that multiplies two numbers
+ *
+ * @argc: input size of argument
+ * @argc: input array of argumen
+ *
+ * Return: 0 for success
+ */
+
+int main(int argc, char *argv[])
+{
+	unsigned int x;
+
+	if (argc == 3)
+	{
+		x = atoi(argv[1]) * atoi(argv[2]);
+		printf("%ld\n", x);
+	}
+	else
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	return (0);
+}
