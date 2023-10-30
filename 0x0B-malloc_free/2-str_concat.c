95e70f0928c1a54bb74d4d4896dd899592f433fd
essaket <hichamessaket17gmail.com>
2023-10-30 13:45:31 -0700

    Done 2-str_concat.c

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -26,7 +26,7 @@ char *str_concat(char *s1, char *s2)
 	while (s2[size2] != '\0')
 		size2++;
 
-	pnt = malloc((size2 + size2) * sizeof(char) + 1);
+	pnt = malloc((size2 + size2) * sizeof(char));
 
 	if (pnt == NULL)
 		return (NULL);
