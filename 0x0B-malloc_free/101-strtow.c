97735bc85c4e796292a10f19462cc8765fb877ea
essaket <hichamessaket17gmail.com>
2023-10-31 13:04:53 -0700

    Done 101-strtow.c

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -10,23 +10,22 @@
  * Return: number of words
  */
 
-int length_words(char *c)
+int length_wors(char *s)
 {
-	int i, x = 0;
-
-	for (i = 0; c[i]; i++)
-	{
-		if (c[i] == ' ')
-		{
-			if (c[i + 1] != ' ' && c[i + 1] != '\0')
-				x++;
-		}
-		else if (i == 0)
-			x++;
-	}
-	x++;
-
-	return (x);
+int flag, c, w;
+flag = 0;
+w = 0;
+for (c = 0; s[c] != '\0'; c++)
+{
+if (s[c] == ' ')
+flag = 0;
+else
+if (flag == 0)
+{flag = 1;
+w++;
+}
+}
+return (w);
 }
 
 /**
