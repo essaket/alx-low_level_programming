564161a02a2a34592829c5d660bc75e0402ea804
essaket <hichamessaket17gmail.com>
2023-10-31 13:47:53 -0700

    Done 101-strtow.c

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -10,22 +10,28 @@
  * Return: number of words
  */
 
-int length_words(char *s)
+int length_words(char *c)
 {
-int flag, c, w;
-flag = 0;
-w = 0;
-for (c = 0; s[c] != '\0'; c++)
-{
-if (s[c] == ' ')
-flag = 0;
-else
-if (flag == 0)
-{flag = 1;
-w++;
-}
-}
-return (w);
+	int i, j, k;
+
+	i = 0;
+	k = 0;
+	for (j = 0; c[j] != '\0'; j++)
+	{
+		if (c[j] == ' ')
+		{
+			i = 0;
+		}
+		else
+		{
+			if (i == 0)
+			{
+				i = 1;
+				k++;
+			}
+		}
+	}
+	return (k);
 }
 
 /**
@@ -36,39 +42,41 @@ return (w);
  * Return: a pointer to an array of strings
  */
 
-char **strtow(char *str)
+har **strtow(char *str)
 {
-char **matrix, *tmp;
-int i, k = 0, len = 0, words, c = 0, start, end;
-while (*(str + len))
-len++;
-words = length_words(str);
-if (words == 0)
-return (NULL);
-matrix = (char **) malloc(sizeof(char *) * (words + 1));
-if (matrix == NULL)
-return (NULL);
-for (i = 0; i <= len; i++)
-{
-if (str[i] == ' ' || str[i] == '\0')
-{
-if (c)
-{
-end = i;
-tmp = (char *) malloc(sizeof(char) * (c + 1));
-if (tmp == NULL)
-return (NULL);
-while (start < end)
-*tmp++ = str[start++];
-*tmp = '\0';
-matrix[k] = tmp - c;
-k++;
-c = 0;
-}
-}
-else if (c++ == 0)
-start = i;
-}
-matrix[k] = NULL;
-return (matrix);
+	char **pnt, *pt;
+	int i, k = 0, len = 0, w, c = 0, s, e;
+
+	while (*(str + len))
+		len++;
+	w = length_words(str);
+	if (w == 0)
+		return (NULL);
+	pnt = (char **) malloc(sizeof(char *) * (w + 1));
+	if (pnt == NULL)
+		return (NULL);
+	for (i = 0; i <= len; i++)
+	{
+		if (str[i] == ' ' || str[i] == '\0')
+		{
+			if (c)
+			{
+				e = i;
+				pt = (char *) malloc(sizeof(char) * (c + 1));
+				if (pt == NULL)
+					return (NULL);
+				while (s < e)
+					*pt++ = str[s++];
+				*pt = '\0';
+				pnt[k] = pt - c;
+				k++;
+				c = 0;
+			}
+		}
+		else if (c++ == 0)
+			s = i;
+	}
+	pnt[k] = NULL;
+
+	return (pnt);
 }
