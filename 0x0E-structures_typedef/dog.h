78f2da3a74fd6624ecff758113e564f361ce0eef
essaket <hichamessaket17gmail.com>
2023-11-04 08:48:22 -0700

    Done dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,21 @@
+#ifndef STRUCT_DOD_H
+#define STRUCT_DOG_H
+
+/**
+ * struct_dog - a structure that containe tree element
+ *
+ * @name: input of function
+ * @age: input of function
+ * @owner: input of function
+ *
+ * Return: 0 for success
+ */
+
+struct dog
+{
+	char *name;
+	float *age;
+	char *owner;
+};
+
+#endif
