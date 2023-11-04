8cdbe66e7fdcdfc28ca4cd64eb9e66207dcd841d
essaket <hichamessaket17gmail.com>
2023-11-04 01:51:56 -0700

    Done 2-main.c

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * main - program that prints the name of the file it was compiled from
+ *
+ * Return: 0 for success
+ */
+
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (NULL);
+}
