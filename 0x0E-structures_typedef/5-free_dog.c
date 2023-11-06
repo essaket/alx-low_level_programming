13b643121a55fa4bc1ae06f126ee08ee5d7ca1eb
essaket <hichamessaket17gmail.com>
2023-11-06 09:25:35 -0800

    Done 5-free_dog.c

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,19 @@
+#include "dog.h"
+
+/**
+ * free_dog - s function that frees dogs
+ *
+ * @d: input pointer of function
+ *
+ * Return: a void
+ */
+
+void free_dog(dog_t *d)
+{
+	if (d)
+	{
+		free(d->name);
+		free(d->owner);
+		free(d);
+	}
+}
