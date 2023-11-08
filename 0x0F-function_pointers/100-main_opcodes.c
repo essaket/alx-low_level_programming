6f9b3e6fa17c13ea2924ad1a8babede0053067e6
essaket <hichamessaket17gmail.com>
2023-11-08 14:10:20 -0800

    Done

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,35 @@
+#include "function_pointers.h"
+
+/**
+ * main - a program that prints the opcodes
+ *
+ * @argc: input arguments of function
+ * @argv: input argument of function
+ *
+ * Return: 0 for success
+ */
+
+int main(int argc, char *argv[])
+{
+	int x;
+	char *pnt = (char *)main;
+
+	if (argc != 2)
+	{
+		printf("Erroe\n");
+		exit(1);
+	}
+
+	x = atoi(argv[1]);
+	if (x < 0)
+	{
+		printf("Error\n");
+		exit(2);
+	}
+
+	do{
+		printf("%02hhx%s", *pnt++, x ? " " : "\n");
+	}while (--b);
+
+	return (0);
+}
