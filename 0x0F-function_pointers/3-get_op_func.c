c550e2bcbc8d0effe2719ececc70c71ec6f3768b
essaket <hichamessaket17gmail.com>
2023-11-08 13:37:07 -0800

    Done 3-main.c, 3-op_functions.c, 3-get_op_func.c, 3-calc.h

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -21,11 +21,10 @@ int (*get_op_func(char *s))(int, int)
 	};
 	int i = 0;
 
-	while (i < 5)
+	for(; i < 5; i++)
 	{
 		if (s && s[0] == ops[i].op[0] && !s[1])
 			return (ops[i].f);
-		i++;
 	}
 
 	return (NULL);
