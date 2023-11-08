d9c2e310b4d822af3846c3961be550a52c97e01b
essaket <hichamessaket17gmail.com>
2023-11-08 12:47:39 -0800

    Done 3-main.c, 3-op_functions.c, 3-get_op_func.c, 3-calc.h

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,42 @@
+#include "3-calc.h"
+
+/**
+ * main - a program that calcule an operation
+ *
+ * @argc: input argc of arguments
+ * @argv: input argv of arguments
+ *
+ * Return: 0 for success
+ */
+
+int main(int argc, char *argv[])
+{
+	int (*op-result)(int, int), a, b;
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+
+	a = atoi(argv[1]);
+	b = atoi(argv[3]);
+
+	op_result = get_op_func(argv[2]);
+
+	if (op_result == NULL)
+	{
+		printf("Error\n");
+		exit(99);
+	}
+
+	if (!b && (argv[2][0] == '/' || argv[2][0] == '%'))
+	{
+		printf("Error\n");
+		exit(100);
+	}
+
+	printf("%d\n", op_result(a, b);
+
+	return (0);
+}
---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,71 @@
+#include "3-calc.h"
+
+/**
+ * op_add - function that sum two integers
+ *
+ * @a: input of function
+ * @b: input of function
+ *
+ * Return: sum of two integers
+ */
+
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - function that difference two integers
+ *
+ * @a: input of function
+ * @b: input of function
+ *
+ * Return: difference of two integers
+ */
+
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - function that product two integers
+ *
+ * @a: input of function
+ * @b: input of function
+ *
+ * Return: product of two integers
+ */
+
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - function that get the result of the division of two integers
+ *
+ * @a: input of function
+ * @b: input of function
+ *
+ * Return: division of two integers
+ */
+
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod - function that remainder of the division of two integers
+ *
+ * @a: input of function
+ * @b: input of function
+ *
+ * Return: module of two integers
+ */
+
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
