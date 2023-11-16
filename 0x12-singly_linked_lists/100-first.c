a1c5675479e5c89020efef2c9f7af3e8028bf6d7
essaket <hichamessaket17gmail.com>
2023-11-16 09:53:46 -0800

    Write task 5. The Hare and the Tortoise

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,13 @@
+#include "lists.h"
+
+void hicham(void) __attribute__ ((hicham));
+
+/**
+ * hicham - funtion that prints a string before the main function is executed
+ */
+
+void hicham(void)
+{
+	printf("You're beat! and yet, you must allow,\n"
+			"I bore my house upon my back!\n");
+}
