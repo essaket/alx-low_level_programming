d713e95af85e9d76174751e9f1e22d03172b412a
essaket <hichamessaket17gmail.com>
2023-11-16 11:12:00 -0800

    Write 101-hello_holberton.asm

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,8 +1,11 @@
- global    main
+section		.text
+	  global    main
           extern    printf
 main:
 	  mov   edi, hicham
 	  xor   eax, eax
 	  call  printf
 	  mov 	eax, 0
-hicham: db `Hello, Holberton\n`,0
+	  ref
+section		.data
+	  hicham db `Hello, Holberton\n`,0
