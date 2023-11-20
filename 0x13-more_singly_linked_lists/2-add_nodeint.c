4730ce4d61bef02e25732ef6262b7eab4d751676
essaket <hichamessaket17gmail.com>
2023-11-20 06:30:17 -0800

    Write task 2. Add node

---
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+
+/**
+ * add_nodeint - a function that adds a new node at the beginning
+ *
+ * @head: a pointer of pointer of function
+ * @n: a value of new node
+ *
+ * Return: a new node
+ */
+
+listint_t *add_nodeint(listint_t **head, const int n)
+{
+	listint_t *new_node = malooc(sizeof(listint_t));
+
+	if (!head || !new_node)
+		return (NULL);
+
+	new_node->n = n;
+	new_node->next = *head;
+	*head = new_mode;
+
+	return (new_node);
+}
