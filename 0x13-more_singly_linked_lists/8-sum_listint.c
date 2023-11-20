0f53c3f19f546959445fdd3136a75ee430764fec
essaket <hichamessaket17gmail.com>
2023-11-20 14:22:41 -0800

    Write task 8. Sum list

--- 0x13-more_singly_linked_lists/8-sum_listint.c
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -1,3 +1,22 @@
 #include "lists.h"
 
+/**
+ * sum_listint - function that returns the sum of all the data
+ *
+ * @head: pointer of function
+ *
+ * Return: the sum of all data
+ */
 
+int sum_listint(listint_t *head)
+{
+	unsigned int sum = 0;
+
+	while (head)
+	{
+		sum += head->n;
+		head = head->next;
+	}
+
+	return (sum);
+}
