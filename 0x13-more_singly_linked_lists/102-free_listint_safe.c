545363414c6250ffc1b45e11f817d5cfa9220242
essaket <hichamessaket17gmail.com>
2023-11-21 14:01:51 -0800

    Write task 13. Free (safe version)

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -10,29 +10,31 @@
 
 size_t free_listint_safe(listint_t **h)
 {
-size_t len = 0;
-int diff;
-listint_t *temp;
-if (!h || !*h)
-return (0);
-while (*h)
-{
-diff = *h - (*h)->next;
-if (diff > 0)
-{
-temp = (*h)->next;
-free(*h);
-*h = temp;
-len++;
-}
-else
-{
-free(*h);
-*h = NULL;
-len++;
-break;
-}
-}
-*h = NULL;
-return (len);
+	size_t size = 0;
+	int x;
+	listint_t *temp;
+
+	if (!h || !*h)
+		return (0);
+	while (*h)
+	{
+		x = *h - (*h)->next;
+		if (x > 0)
+		{
+			temp = (*h)->next;
+			free(*h);
+			*h = node;
+			size++;
+		}
+		else
+		{
+			free(*h);
+			*h = NULL;
+			size++;
+			break;
+		}
+	}
+	*h = NULL;
+
+	return (size);
 }
