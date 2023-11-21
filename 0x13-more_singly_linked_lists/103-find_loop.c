52a22c721be908e93483625df09271a0b9a6f4bc
essaket <hichamessaket17gmail.com>
2023-11-21 14:13:07 -0800

    Write task 14. Find the loop

---
+++ 0x13-more_singly_linked_lists/103-find_loop.c
@@ -0,0 +1,35 @@
+#include "lists.h"
+
+/**
+ * find_listint_loop - function that finds the loop in a linked list
+ *
+ * @head: address to pointer of function
+ *
+ * Return: the address or NULL
+ */
+
+listint_t *find_listint_loop(listint_t *head)
+{
+	listint_t *s = head, *f = head;
+
+	if (!head)
+		return (NULL);
+
+	while (s && f && f->next)
+	{
+		f = f->next->next;
+		s = s->next;
+		if (f == s)
+		{
+			s = head;
+			while (s != f)
+			{
+				s = s->next;
+				f = f->next;
+			}
+			return (f);
+		}
+	}
+
+	return(NULL);
+}
