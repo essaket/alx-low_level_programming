bde6ab9d6351d08c89cc88c9b5c9fa8af3a5e6a7
essaket <hichamessaket17gmail.com>
2023-11-23 12:16:23 -0800

    Write task 4. 100

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -13,7 +13,7 @@ int clear_bit(unsigned long int *n, unsigned int index)
 {
 	unsigned int x = 1;
 
-	if (index >= (sizeof(n) * 8))
+	if (index >= sizeof(n) * 8)
 		return (-1);
 	x <<= index;
 	*n ^= x;
