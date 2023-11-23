a5c45ad368e4685d8b388876af26f1546c916075
essaket <hichamessaket17gmail.com>
2023-11-23 09:47:30 -0800

    Write task 0. 0

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -18,8 +18,7 @@ unsigned int binary_to_uint(const char *b)
 	{
 		if (*b != '0' && *b != '1')
 			return (0);
-		nmb = nmb * 2 + (*b - '0');
-		*b++;
+		nmb = nmb * 2 + (*b++ - '0');
 	}
 
 	return (nmb);
