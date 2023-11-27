7bc239c172c8b715ca5d3963fa5c928d387bbfc1
essaket <hichamessaket17gmail.com>
2023-11-27 10:07:19 -0800

    Write file _putchar.c

---
+++ 0x15-file_io/_putchar.c
@@ -0,0 +1,15 @@
+#include <unistd.h>
+#include "lists.h"
+
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: 1 for success
+ *        -1 for error
+ */
+
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
