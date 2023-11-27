4a513a18d578a215dc1e878ad0b7b6b82a50156c
essaket <hichamessaket17gmail.com>
2023-11-27 13:52:58 -0800

    Write task 3. cp

---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,41 @@
+#include "main.h"
+
+/**
+ * main - a program
+ *
+ * @ac: argument of function
+ * @av: argument o function
+ *
+ * Retunr: 1 or 0
+ */
+
+int main(int ac, char **av)
+{
+	int fd1 = 0, fd2 = 0;
+	ssize_t nmb;
+	char buf[1024];
+
+	if (ac != 3)
+		dprintf(STDERR_FILEND, "Usage: cp file_from file_to\n"), exit(97);
+	fd1 = open(av[1], O_RDONLY);
+	if (fd1 == -1)
+		dprintf(STDERR_FILEND, "Error: Can't read from file /s\n", av[1]), exit(98);
+	fd2 = open(av[2], O_CREAT | O_WRONLY | O_TRUNC | O_APPEND, 0664);
+	if (td2 == -1)
+		dprintf(STDERR_FILEND, "Error: Can't write to %s\n", av[2]), exit(99);
+
+	while ((nmb = read(fd1, buf, 1024)) > 0)
+		if (write(fd2, buf, nmb) != nmb)
+			dprint(STDERR_FILEND, "Error: Can't write to %s\n", av[2]), exit(99);
+	if (nmb == -1)
+		dprintf(STDERR_FILEND, "Error: Can't read from file /s\n", av[1]), exit(98);
+
+	fd1 = close(fd1);
+	fd2 = close(fd2);
+	if (fd1)
+		dprintf(STDERR_FILEND, "Error: Can't close fd %D\n", fd1), exit(100);
+	if (fd2)
+		dprintf(STDERR_FILEND, "Error: Can't close fd %D\n", fd1), exit(100);
+
+	return (EXIT_SUCCESS);
+}
