6ee9d92d1de98f8331dd5101c8c055cc52c58fb1
essaket <hichamessaket17gmail.com>
2023-11-27 14:27:26 -0800

    Write task 3. cp

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -16,26 +16,26 @@ int main(int ac, char **av)
 	char buf[1024];
 
 	if (ac != 3)
-		dprintf(STDERR_FILEND, "Usage: cp file_from file_to\n"), exit(97);
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n"), exit(97);
 	fd1 = open(av[1], O_RDONLY);
 	if (fd1 == -1)
-		dprintf(STDERR_FILEND, "Error: Can't read from file /s\n", av[1]), exit(98);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]), exit(98);
 	fd2 = open(av[2], O_CREAT | O_WRONLY | O_TRUNC | O_APPEND, 0664);
-	if (td2 == -1)
-		dprintf(STDERR_FILEND, "Error: Can't write to %s\n", av[2]), exit(99);
+	if (fd2 == -1)
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]), exit(99);
 
 	while ((nmb = read(fd1, buf, 1024)) > 0)
 		if (write(fd2, buf, nmb) != nmb)
-			dprint(STDERR_FILEND, "Error: Can't write to %s\n", av[2]), exit(99);
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]), exit(99);
 	if (nmb == -1)
-		dprintf(STDERR_FILEND, "Error: Can't read from file /s\n", av[1]), exit(98);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]), exit(98);
 
 	fd1 = close(fd1);
 	fd2 = close(fd2);
 	if (fd1)
-		dprintf(STDERR_FILEND, "Error: Can't close fd %D\n", fd1), exit(100);
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd1), exit(100);
 	if (fd2)
-		dprintf(STDERR_FILEND, "Error: Can't close fd %D\n", fd1), exit(100);
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd1), exit(100);
 
 	return (EXIT_SUCCESS);
 }
