6b6e1edfa8b6b5ce407032aeb7dc815e88fcafbc
essaket <hichamessaket17gmail.com>
2023-11-27 11:05:19 -0800

    Write task 1. Under the snow

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -12,14 +12,14 @@
 int create_file(const char *filename, char *text_content)
 {
 	int fd;
-	ssize_t nmb = 0, len = 0;
+	ssize_t nmb = 0, len;
 
 	if (!filename)
 		return (-1);
 	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
 	if (fd == -1)
 		return (-1);
-	for (; text_content[len]; len++)
+	for (len = 0; text_content[len]; len++)
 		;
 	if (len)
 		nmb = write(fd, text_content, len);
