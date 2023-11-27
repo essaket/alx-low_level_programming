5fa4dd7f7308cab14eb1e0746d07d77619d07f6d
essaket <hichamessaket17gmail.com>
2023-11-27 12:17:29 -0800

    Write task 2. Speak gently, she can hear

---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * append_text_to_file - function that apprends text at the end of a file
+ *
+ * @filename: input pointer of function
+ * @text_content: input pointer of function
+ *
+ * Return: 1 for success or -1 if not
+ */
+
+int append_text_to_file(const char *filename, char *text_content)
+{
+	int fd, nmb, len = 0;
+
+	if (!filename)
+		return (-1);
+	fd = open(filename, O_WRONLY | O_APPEND);
+	if (fd == -1)
+		return (-1);
+	if (text_content)
+	{
+		for (; text_content[len]; len++)
+			;
+		nmb = write(fd, text_content, len);
+	}
+	clode(fd);
+
+	return (nmb == len ? 1 : -1);
+}
