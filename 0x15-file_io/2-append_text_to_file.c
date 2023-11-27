1d487a3aa63acbb42283233b3f29c405d14bacad
essaket <hichamessaket17gmail.com>
2023-11-27 12:25:08 -0800

    Write task 2. Speak gently, she can hear

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -24,7 +24,9 @@ int append_text_to_file(const char *filename, char *text_content)
 			;
 		nmb = write(fd, text_content, len);
 	}
+	if (nmb == -1)
+		return (-1);
 	close(fd);
 
-	return (nmb == len ? 1 : -1);
+	return (1);
 }
