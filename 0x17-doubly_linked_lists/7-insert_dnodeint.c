005abd2f0c674e248c7684b4d7b2e03bc0d0d99d
essaket <hichamessaket17gmail.com>
2024-01-09 10:12:51 -0800

    writing task 7

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -31,8 +31,8 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	new_node->n = n;
 	new_node->next = node->next;
 	new_node->prev = node;
-	node->next = new_node;
 	node->next->prev = new_node;
+	node->next = new_node;
 
 	return (new_node);
 }
