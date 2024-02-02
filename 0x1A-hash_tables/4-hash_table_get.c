40ef7213cbb3fb74f2d4b68b3076b9dfdd9ed725
essaket <hichamessaket17gmail.com>
2024-02-02 09:17:14 -0800

    Writing Task 4

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -5,7 +5,7 @@
  * @ht: the hash table you want to look into
  * @key: the key you are looking for
  *
- * Return: the value associated with the element, or NULL if key couldn’t be found
+ * Return: value associated with the element, or NULL if key couldn’t found
  */
 
 char *hash_table_get(const hash_table_t *ht, const char *key)
