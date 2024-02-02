f13a366d66a0b083a5f7e5dad20dd3e20e38a5b6
essaket <hichamessaket17gmail.com>
2024-02-02 08:31:27 -0800

    Writing Task 2

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -8,7 +8,7 @@
  * Return: index for the key
  */
 
-int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
 	if (size == 0)
 		return (0);
